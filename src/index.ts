664bda97c0337c576bc51663e696f32fdd4f3ee0
Jared Forsyth <jared@jaredforsyth.com>
2026-05-14 15:43:40 -0500

    move

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,14 @@
-export * from './core';
+export {
+    createPatchDispatcher, DraftPatch,
+    createPatchBuilder,
+    type ApplyTiming,
+    type Patch, type Path,
+    type PatchBuilder,
+} from './core';
 export {
     blankHistory,
     dispatch,
     jump,
+    type Annotations,
+    type History,
 } from './history/history';
-export type {
-    Annotations,
-    History,
-} from './history/history';
-export {
-    createHistoryContext,
-    createStateContext,
-    useValue,
-} from './react/react';
-export type {Extra, Updater} from './react/react';
